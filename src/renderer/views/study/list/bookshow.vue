1314ba3069659fc153b587cbe9fa1c1c2afb211a
sun <sun@example.com>
2019-08-01 15:29:17 +0800

    1

--- src/renderer/views/study/list/bookshow.vue
+++ src/renderer/views/study/list/bookshow.vue
@@ -5,37 +5,48 @@
 			<el-col :span="20" :offset="2" justify="center" align="center">
 				<div class="" style="margin-top: 15px;">
 					<span>{{program.title}}</span>
-					<el-tag type="info" @click.native="gotoback"  class="backbutton"> 返回</el-tag>
+					<el-tag type="info" @click.native="gotoback" class="backbutton"> 返回</el-tag>
 				</div>
 			</el-col>
 		</el-row>
-		
-		
+
+
 		<el-row v-if="taskin">
 			<el-col :span="20" :offset="2" justify="left" align="left">
 				<div class="" style="margin-top: 15px;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);padding: 10px;font-size: 12px; line-height: 20px;">
-					
+
 					<p v-if="program.author!=''">作者：{{program.author}}</p>
 					<p v-if="program.subject!=''">主题：{{program.subject}}</p>
-    <p v-if="program.description!=''">简介：{{program.description}}</p>
+					<p v-if="program.description!=''">简介：{{program.description}}</p>
 				</div>
 			</el-col>
 		</el-row>
-		
-		
-			<el-row v-if="taskin" v-for="(slide, index) in data" :key="index">
-				<el-col :span="20" :offset="2" justify="left" align="left">
-					<div class="" style="margin-top: 15px;">
-						<span><el-tag type="warning">{{slide.name}}</el-tag></span>
-						<el-divider></el-divider>
-						<template v-for="sub in slide.wcell_list">
-						
-						<span > {{sub.word}}</span> <el-divider direction="vertical"></el-divider>
-						 </template>
-					</div>
-				</el-col>
-			</el-row>
-	
+
+
+		<el-row v-if="taskin" v-for="(slide, index) in data" :key="index">
+			<el-col :span="20" :offset="2" justify="left" align="left">
+				<div class="" style="margin-top: 15px;">
+					<span>
+						<el-tag type="warning">{{slide.obj.name}}</el-tag>
+					</span>
+					<el-divider></el-divider>
+
+					<el-tabs v-model="activetablist[index].wcell_name" type="border-card">
+					  
+						<el-tab-pane v-for="(sub, index2) in slide.wcell_list" :key="index2" :label="sub.name" :name="sub.md5">
+							<el-tag v-for="(sub2, index3) in sub.data" :key="index3">{{sub2.word}}</el-tag>
+								
+								<p> <el-button type="warning">开始识字</el-button></p>
+								 
+						</el-tab-pane>
+
+					</el-tabs>
+					<!-- <el-tag type="info" v-for="(sub, index2) in slide.wcell_list" :key="index2">{{sub.name}}</el-tag> -->
+					<!-- 	<span > {{sub.word}}</span> <el-divider direction="vertical"></el-divider> -->
+				</div>
+			</el-col>
+		</el-row>
+
 	</div>
 </template>
 
@@ -51,32 +62,37 @@
 				ebookid: 0,
 				program: null,
 				data: null,
-				taskin:false,
+				taskin: false,
+				activeName: 'first',
+				activetablist:null
 			}
 		},
 		filters: {
 
 		},
 		created() {
-			
+
 			this.ebookid = this.$route.query.ebookid
 			// this.ebookid = 446654
-			console.log(this.ebookid );
+			// console.log(this.ebookid);
 			this.init()
-   
+			
+			
 		},
 		methods: {
-				 gotoback(a) {
-					 // console.log(a);
-					this.$router.replace({ name: 'Book'})
-				},
+			gotoback(a) {
+				// console.log(a);
+				this.$router.replace({
+					name: 'Book'
+				})
+			},
 			fetchData() {
 				const loading = this.$loading({
-							  lock: true,
-							  text: '加载中...',
-							  spinner: 'el-icon-loading',
-							  background: '#000'
-							});
+					lock: true,
+					text: '加载中...',
+					spinner: 'el-icon-loading',
+					background: '#000'
+				});
 
 				const params = {
 					ebookid: this.ebookid,
@@ -85,9 +101,15 @@
 					loading.close();
 					this.data = response.items
 					this.program = response.program
+					this.activetablist = JSON.parse(JSON.stringify(response.datas_tab)); 
 					console.log(response);
 					this.taskin = true
+					
+					console.log(this.activetablist);
+					
 				})
+				
+					
 			},
 
 			init() {
